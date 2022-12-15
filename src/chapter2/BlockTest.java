b5eb44c45061e54a7549c69d8e0e073ac599be6c
ManSerobyan <manikserobyan8@mail.com>
2022-12-15 21:36:49 +0400

    Chapter 2 examples

--- src/chapter2/BlockTest.java
+++ src/chapter2/BlockTest.java
@@ -8,6 +8,7 @@ public class BlockTest {
 
         for(x = 0; x<10; x++) {
             System.out.println("Значение x:" + x);
+
             System.out.println("Значение y:" + y);
             y = y - 2;
         }
