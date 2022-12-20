0e09965572986f9707d6a62d76971e6843b42d4b
ManSerobyan <manikserobyan8@mail.com>
2022-12-21 02:41:53 +0400

    Figure example

--- src/homeworks/homework1/FigurePainter.java
+++ src/homeworks/homework1/FigurePainter.java
@@ -11,29 +11,27 @@ public class FigurePainter {
             }
             System.out.println();
         }
-
         System.out.println();
-
-
-        //Figure 2
-        for (int i = 5; i > 0; i--) {
-            for (int j = 0; j < i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int m = 5; m > i - 1; m--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
         //Figure 3
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            System.out.println();
-            for (int j = i; j <= n; j++) {
-                System.out.print(" * ");
-            }
-        }
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //Figure 2
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
     }
 }
