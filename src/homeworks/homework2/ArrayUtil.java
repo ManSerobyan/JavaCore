1405258c7a4a454c96238a020568733e7a7d1309
ManSerobyan <manikserobyan8@mail.com>
2022-12-26 01:57:08 +0400

    Array Util examples

--- src/homeworks/homework2/ArrayUtil.java
+++ src/homeworks/homework2/ArrayUtil.java
@@ -24,8 +24,56 @@ public class ArrayUtil {
         System.out.println("max= " + max);
         System.out.println("min= " + min);
 
+        System.out.println();
+
+
+        System.out.println("last element: " + array[array.length-1]);
+
+        System.out.println("first element: " + array[0]);
+
+        System.out.println("middle element: " + array[array.length/2]);
+
+        System.out.println();
+
+        System.out.println();
+        int countCouple = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)  {
+                System.out.print(array[i] + " ");
+                countCouple++;
+            }
+        }
+        System.out.println();
+        System.out.println("Count Couple = " + countCouple);
+
+        System.out.println();
+        int  countOdd = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0){
+                System.out.print(array[i] + " ");
+                countOdd++;
+            }
 
         }
+        System.out.println();
+        System.out.println("Count Odd = " + countOdd);
+
+        System.out.println();
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("sum = " + sum);
+
+        System.out.println("The arithmetic average = " + sum / array.length);
+
     }
+
+}
+
+
+
+
+
 
 
