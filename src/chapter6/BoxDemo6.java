13daf0d812924a9fe5b0ef34bcb8ac7535b58eda
ManSerobyan <manikserobyan8@mail.com>
2023-01-16 00:31:43 +0400

    chapter 6 examples

---
+++ src/chapter6/BoxDemo6.java
@@ -0,0 +1,17 @@
+package chapter6;
+
+public class BoxDemo6 {
+
+    public static void main(String[] args) {
+        Box myBox1 = new Box();
+        Box myBox2 = new Box();
+
+        double vol;
+
+        vol = myBox1.volume(0);
+        System.out.println(vol);
+
+        vol = myBox2.volume(0);
+        System.out.println(vol);
+    }
+}
